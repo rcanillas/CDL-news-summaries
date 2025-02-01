c9393a59248bd348f5172255e59de44e43f907dd
Rémi Canillas <remi.canillas@gmail.com>
2025-02-01 16:11:07 +0100

    first POC news retrieval

--- modules/news_getter.py
+++ modules/news_getter.py
@@ -1,12 +1,26 @@
 import requests
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from pprint import pprint
+
 load_dotenv()
-print(os.environ["NEWSDATA_KEY"])
 
 
 class NewsGetter:
 
-    def __init__(api_key):
-        self.api_key = api_key
+    def __init__(self):
+        self.api_key = os.environ["NEWSDATA_KEY"]
+        self.url = f"https://newsdata.io/api/1/latest"
+
+    def get_tech_articles(self):
+        headers = {"X-ACCESS-KEY": self.api_key}
+        params = {"category": "technology", "language": "fr,en", "size": 10}
+        tech_articles = requests.get(self.url, headers=headers, params=params).json()
+        pprint(tech_articles)
+        return tech_articles
+
+
+if __name__ == "__main__":
+    newsGetter = NewsGetter()
+    tech_articles = newsGetter.get_tech_articles()
